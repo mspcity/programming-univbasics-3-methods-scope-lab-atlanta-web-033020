72c600f9336cadb41ae0fa15e1152b8f3083f626
Youngmin Park <codepark2019@gmail.com>
2020-03-17 07:44:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,6 +6,10 @@ def toadstool
   puts status = "Thank You Mario! But Our Princess Is In Another Castle!"
 end
 
-def link
-  puts phrase = "It's Dangerous To Go Alone! Take This."
-end
+# def link
+#   puts phrase = "It's Dangerous To Go Alone! Take This."
+# end
+
+# def all_phrases
+#   puts
+# end
